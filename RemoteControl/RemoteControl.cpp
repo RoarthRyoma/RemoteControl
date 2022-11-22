7b406438e197278bb360561b056a9d001cb6f912
Roarth <1420862133@qq.com>
2022-11-22 19:08:59 +0800

    完成了《获取指定文件目录下的文件和文件夹》功能

--- RemoteControl/RemoteControl.cpp
+++ RemoteControl/RemoteControl.cpp
@@ -4,8 +4,10 @@
 #include "pch.h"
 #include "framework.h"
 #include "RemoteControl.h"
-#include "ServerSocket.h";
+#include "ServerSocket.h"
 #include <direct.h>
+#include <io.h>
+#include <list>
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -32,6 +34,24 @@ void Dump(BYTE* pData, size_t nSize)
     OutputDebugStringA(strOut.c_str());
 }
 
+typedef struct _FILE_INFO
+{
+    _FILE_INFO()
+    {
+        IsInvalid = FALSE;
+        IsDirectory = -1;
+        HasNext = TRUE;
+        memset(szFileName, 0, sizeof(szFileName));
+    }
+
+    BOOL IsInvalid;//是否有效, 0-否 1-是
+    BOOL IsDirectory;//是否为目录, 0-否 1-是
+    BOOL HasNext;   //是否还有后续
+    char szFileName[256];//文件名
+
+} FILEINFO, *PFILEINFO;
+
+//获取磁盘信息
 int MakeDriveInfo()//1->A  2->B  3->C ... 26->Z
 {
     std::string result;
@@ -47,6 +67,55 @@ int MakeDriveInfo()//1->A  2->B  3->C ... 26->Z
     Dump((BYTE*)pack.Data(), pack.Size());
     //CServerSocket::getInstance()->Send(CPacket(1, (BYTE*)result.c_str(), result.size()));
     //CServerSocket::getInstance()->Send(pack);
+    return 0;
+}
+
+//获取磁盘分区目录
+int MakeDirectoryInfo()
+{
+    std::string strPath;
+    //std::list<FILEINFO> lstFileInfo;
+    bool flag = CServerSocket::getInstance()->GetFilePath(strPath);
+    if (!flag)
+    {
+        OutputDebugString(_T("命令解析错误！当前命令不是获取文件列表。"));
+        return -1;
+    }
+    if (_chdir(strPath.c_str()) != 0)
+    {
+        FILEINFO finfo;
+        finfo.IsInvalid = TRUE;
+        finfo.IsDirectory = TRUE;
+        finfo.HasNext = FALSE;
+        memcpy(finfo.szFileName, strPath.c_str(), strPath.size());
+        //lstFileInfo.push_back(finfo);
+        CPacket pack(2, (BYTE*)&finfo, sizeof(finfo));
+        CServerSocket::getInstance()->Send(pack);
+        OutputDebugString(_T("没有权限访问当前目录。"));
+        return -2;
+    }
+    _finddata_t fdata;
+    int hfind = 0;
+    if ((hfind = _findfirst("*", &fdata)) == -1)// * 匹配所有文件
+    {
+		OutputDebugString(_T("没有找到任何文件。"));
+		return -3;
+    }
+    do 
+    {
+        FILEINFO finfo;
+        finfo.IsDirectory = (fdata.attrib & _A_SUBDIR) != 0;
+        memcpy(finfo.szFileName, fdata.name, strlen(fdata.name));
+		//lstFileInfo.push_back(finfo);
+		CPacket pack(2, (BYTE*)&finfo, sizeof(finfo));
+		CServerSocket::getInstance()->Send(pack);
+    } while (!_findnext(hfind, &fdata));
+    //发送信息到控制端
+    FILEINFO finfo;
+    finfo.HasNext = FALSE;
+	CPacket pack(2, (BYTE*)&finfo, sizeof(finfo));
+	CServerSocket::getInstance()->Send(pack);
+
     return 0;
 }
 
@@ -115,6 +184,9 @@ int main()
             {
             case 1://查看磁盘分区信息
                 MakeDriveInfo();
+                break;
+            case 2://查看分区目录信息
+                MakeDirectoryInfo();
                 break;
             default:
                 break;
