c76806a848c3da32f2bc7c3b8a34686063584e89
Roarth <1420862133@qq.com>
2023-03-14 23:13:35 +0800

    1 完成了UDP打洞的基本原理演示代码

--- RemoteControl/RemoteControl.cpp
+++ RemoteControl/RemoteControl.cpp
@@ -80,28 +80,152 @@ void Iocp()
 	getchar();
 }
 
+void initsock()
+{
+	WSADATA wsa;
+	WSAStartup(MAKEWORD(2, 2), &wsa);
+}
+
+void clearsock()
+{
+	WSACleanup();
+}
+
 void udp_server()
 {
 	printf("%s(%d):%s\r\n", __FILE__, __LINE__, __FUNCTION__);
-	getchar();
+	SOCKET sock = socket(PF_INET, SOCK_DGRAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		printf("%s(%d):%s ERROR(%d)!!!\r\n", __FILE__, __LINE__, __FUNCTION__, WSAGetLastError());
+		return;
+	}
+	std::list<sockaddr_in> lstclients;
+	sockaddr_in server, client;
+	memset(&server, 0, sizeof(server));
+	memset(&client, 0, sizeof(client));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(20000);
+	//server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	std::string ip = "127.0.0.1";
+	inet_pton(AF_INET, ip.c_str(), &server.sin_addr.S_un.S_addr);
+	if (-1 == bind(sock, (sockaddr*)&server, sizeof(server)))
+	{
+		printf("%s(%d):%s ERROR(%d)!!!\r\n", __FILE__, __LINE__, __FUNCTION__, WSAGetLastError());
+		closesocket(sock);
+		return;
+	}
+	std::string buff;
+	buff.resize(1024 * 256);
+	memset((char*) buff.c_str(), 0, buff.size());
+	int len = sizeof(client);
+	int ret = 0;
+	while (!_kbhit())
+	{
+		ret = recvfrom(sock, (char*)buff.c_str(), buff.size(), 0, (sockaddr*)&client, &len);
+		if (ret > 0)
+		{
+			if (lstclients.size() <= 0)
+			{
+				lstclients.push_back(client);
+				printf("%s(%d):%s, IP %08X port %d\r\n", __FILE__, __LINE__, __FUNCTION__, client.sin_addr.s_addr, ntohs(client.sin_port));
+				ret = sendto(sock, buff.c_str(), ret, 0, (sockaddr*)&client, len);
+				printf("%s(%d):%s\r\n", __FILE__, __LINE__, __FUNCTION__);
+			}
+			else
+			{
+				memcpy((void*)buff.c_str(), &lstclients.front(), sizeof(lstclients.front()));
+				ret = sendto(sock, buff.c_str(), sizeof(lstclients.front()), 0, (sockaddr*)&client, len);
+				printf("%s(%d):%s\r\n", __FILE__, __LINE__, __FUNCTION__);
+			}
+			//CEdoyunTool::Dump((BYTE*)buff.c_str(), ret);
+		}
+		else
+		{
+			printf("%s(%d):%s ERROR(%d)!!! ret=%d\r\n", __FILE__, __LINE__, __FUNCTION__, WSAGetLastError(), ret);
+		}
+	}
+	closesocket(sock);
+	printf("%s(%d):%s\r\n", __FILE__, __LINE__, __FUNCTION__);
 }
 
 void udp_client(bool isHost = true)
 {
-	if (isHost)
+	Sleep(2000);
+	sockaddr_in server, client;
+	int len = sizeof(client);
+	memset(&server, 0, sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(20000);
+	//server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	std::string ip = "127.0.0.1";
+	inet_pton(AF_INET, ip.c_str(), &server.sin_addr.S_un.S_addr);
+	SOCKET sock = socket(PF_INET, SOCK_DGRAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		printf("%s(%d):%s ERROR(%d)!!!\r\n", __FILE__, __LINE__, __FUNCTION__, WSAGetLastError());
+		return;
+	}
+	if (isHost)	//主客户端代码
 	{
 		printf("%s(%d):%s\r\n", __FILE__, __LINE__, __FUNCTION__);
+		std::string msg = "Hello world!\n";
+		int ret = sendto(sock, msg.c_str(), msg.size(), 0, (sockaddr*)&server, sizeof(server));
+		printf("%s(%d):%s ret = %d\r\n", __FILE__, __LINE__, __FUNCTION__, ret);
+		if (ret > 0)
+		{
+			msg.resize(1024);
+			memset((char*)msg.c_str(), 0, msg.size());
+			ret = recvfrom(sock, (char*)msg.c_str(), msg.size(), 0, (sockaddr*)&client, &len);
+			printf("host: %s(%d):%s ERROR(%d)!!! ret=%d\r\n", __FILE__, __LINE__, __FUNCTION__, WSAGetLastError(), ret);
+			if (ret > 0)
+			{
+				printf("%s(%d):%s, IP %08X port %d\r\n", __FILE__, __LINE__, __FUNCTION__, client.sin_addr.s_addr, ntohs(client.sin_port));
+				printf("%s(%d):%s msg = %d\r\n", __FILE__, __LINE__, __FUNCTION__, msg.size());
+			}
+
+			ret = recvfrom(sock, (char*)msg.c_str(), msg.size(), 0, (sockaddr*)&client, &len);
+			printf("host: %s(%d):%s ERROR(%d)!!! ret=%d\r\n", __FILE__, __LINE__, __FUNCTION__, WSAGetLastError(), ret);
+			if (ret > 0)
+			{
+				printf("%s(%d):%s, IP %08X port %d\r\n", __FILE__, __LINE__, __FUNCTION__, client.sin_addr.s_addr, ntohs(client.sin_port));
+				printf("%s(%d):%s msg = %s\r\n", __FILE__, __LINE__, __FUNCTION__, msg.c_str());
+			}
+		}
 	}
-	else
+	else		//从客户端代码
 	{
 		printf("%s(%d):%s\r\n", __FILE__, __LINE__, __FUNCTION__);
+		std::string msg = "Hello world!\n";
+		int ret = sendto(sock, msg.c_str(), msg.size(), 0, (sockaddr*)&server, sizeof(server));
+		printf("%s(%d):%s ret = %d\r\n", __FILE__, __LINE__, __FUNCTION__, ret);
+		if (ret > 0)
+		{
+			msg.resize(1024);
+			memset((char*)msg.c_str(), 0, msg.size());
+			ret = recvfrom(sock, (char*)msg.c_str(), msg.size(), 0, (sockaddr*)&client, &len);
+			printf("client: %s(%d):%s ret = %d\r\n", __FILE__, __LINE__, __FUNCTION__, ret);
+			if (ret > 0)
+			{
+				sockaddr_in addr;
+				memcpy(&addr, msg.c_str(), sizeof(addr));
+				sockaddr_in* paddr = &addr;
+				printf("%s(%d):%s, IP %08X port %d\r\n", __FILE__, __LINE__, __FUNCTION__, client.sin_addr.s_addr, ntohs(client.sin_port));
+				printf("%s(%d):%s msg = %d\r\n", __FILE__, __LINE__, __FUNCTION__, msg.size());
+				printf("%s(%d):%s, IP %08X port %d\r\n", __FILE__, __LINE__, __FUNCTION__, paddr->sin_addr.s_addr, ntohs(paddr->sin_port));
+				msg = "i am a client!";
+				ret = sendto(sock, (char*)msg.c_str(), msg.size(), 0, (sockaddr*)paddr, sizeof(sockaddr_in));
+				printf("client: %s(%d):%s ret = %d\r\n", __FILE__, __LINE__, __FUNCTION__, ret);
+			}
+		}
 	}
+	closesocket(sock);
 }
 
 int main(int argc, char* argv[])
 {
 	if (!CEdoyunTool::Init()) return 1;
-
+	initsock();
 	if (argc == 1)
 	{
 		char wstrDir[MAX_PATH];
@@ -139,6 +263,7 @@ int main(int argc, char* argv[])
 	{
 		udp_client(false);
 	}
+	clearsock();
 
 	//Iocp();
 
